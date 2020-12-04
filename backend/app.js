635a12787e673a9e6e012c3a1df2829b321da5ce
John Doe <laurent.jordan@outlook.fr>
2020-12-04 16:16:52 +0100

    debut du projet

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+// ajout du framework express  nous permettant de déployer nos API beaucoup plus rapidement
+const express = require('express');
+//pour gerer la demande post provenant de l'application front end
+const bodyParser = require('body-parser');
+const path = require('path');
+// integrons la couche de base de donné de notre serveur mongodg
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://jordanL:Jojodu45@thehottestreviews.jiytn.mongodb.net/thehottestreviews?retryWrites=true&w=majority', {
+        userNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('connexion a MongoDB réussie!'))
+    .catch(() => console.log('connexion a MongoDB echoue!'));
+
+
+// Création de l'application express
+const app = express();
+
+//Erreurs de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Rend les données du corps de la requête exploitable
+app.use(bodyParser.json());
+
+// Routes attendues pour les differentes API
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ backend/routes/sauces.js
@@ -0,0 +1,18 @@
+// ajout du framework express  nous permettant de déployer nos API beaucoup plus rapidement
+const express = require('express');
+const router = express.Router();
+// Pour pouvoir utiliser notre nouveau modele mongoose dans l'applucation models/sauces.js
+const sauceCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+// Toutes les routes des API
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+
+router.post('/sauces', auth, multer, sauceCtrl.createSauce);
+
+
+module.exports = router;
